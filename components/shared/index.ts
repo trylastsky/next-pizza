1981ddd6509cd19b90992c2819913e23da6c32be
TRYLASTSKY <johndoe@example.com>
2025-05-01 11:43:27 +0400

    added header and filters component

---
+++ components/shared/index.ts
@@ -0,0 +1,8 @@
+export { Container } from "./container";
+export { Header } from "./header";
+export { Title } from "./title";
+export { Categories } from "./top-bar/components/categories";
+export { SortPopup } from "./top-bar/components/sort-popup";
+export { TopBar } from "./top-bar/TopBar"
+export { FilterCheckbox } from "./filters/filter-checkbox";
+export { Filters } from "./filters/filters";
